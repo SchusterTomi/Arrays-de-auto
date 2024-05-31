323f2ba0f956afbb4c39e04b962fc234e781ba32
SchusterTomi <tomas.schusterman@gmail.com>
2024-05-31 09:08:39 -0300

    Ejercicios 2 Completo

--- Assets/Scripts/AlquilerVehiculos.cs
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AlquilerVehiculos : MonoBehaviour
 {
-    public Vehiculo[] vehiculos;
+    public GameObject[] vehiculos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,25 @@ public class AlquilerVehiculos : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-
+            ResetearVehiculos();
+            vehiculos[Random.Range(0, vehiculos.Length - 1)].SetActive(true);
+            //Se desactiven todos los vehiculos
+            //Y se active un vehivulo al azar del array
         }   
     }
 
     void ResetearVehiculos()
     {
-
+        //desactivar todos los elementos del array
+        DesactivarTodosLosElementos();
+        //activar solo el primer elemento
+        vehiculos[0].SetActive(true);
+    }
+    void DesactivarTodosLosElementos()
+    {
+        for (int i = 0; i<vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
     }
 }
